888cdc3b17691b560b65fd84586b90d665940034
unknown <jj950310@naver.com>
2021-08-14 21:08:57 +0900

    Add model.py

---
+++ DCGAN/model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.conv1 = nn.
+        self.norm1 = 
+        
+        self.conv2 = 
+        self.norm2 = 
+
+        self.conv3 = 
+        self.norm3 = 
+
+        self.conv4 = 
+        self.norm4 = 
+
+        self.conv5 = 
+
+        self.relu = nn.ReLU(True)
+        self.tanh = nn.Tanh()
