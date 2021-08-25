009701dd1d7ba9050ef7332a3e039953af90ac5f
unknown <jj950310@naver.com>
2021-08-26 03:01:50 +0900

    Add init_weights, save_checkpoints

--- DCGAN/utils.py
+++ DCGAN/utils.py
@@ -2,5 +2,26 @@ import os
 import shutil
 import torch
 
-def init_weights():
+def init_weights(m):
     
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weigth.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0)
+
+def save_checkpoints(state, checkpoint, Gen):
+    
+    if not os.path.exists(checkpoint):
+        os.mkdir(checkpoint)
+    else :
+        pass
+
+    if Gen :
+        filepath = os.path.join(checkpoint, 'last_Gen.pth')
+    else :
+        filepath = os.path.join(checkpoint, 'last_Dis.pth')
+
+    torch.save(state, filepath)
