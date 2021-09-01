ed0632f53a93441a2c01617139db7539a27babd0
unknown <jj950310@naver.com>
2021-09-02 02:54:42 +0900

    Add train.py

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -3,13 +3,14 @@ import argparse
 import logging
 
 import torch
+from torch._C import device
 import torch.nn as nn
 
 from model import *
 from dataset import *
 from utils import *
 
-def train():
+def train(dataloader):
 
     G = Generator(out_channel = nch).to(device)
     D = Discriminator(in_channel = nch).to(device)
@@ -21,3 +22,14 @@ def train():
 
     optimizer_G = torch.optim.Adam(G.parameters(), lr = lr, betas = (beta1, 0.999))
     optimizer_D = torch.optim.Adam(D.parameters(), lr = lr, betas = (beta1, 0.999))
+
+    real_label = 1
+    fake_label = 0
+
+    for epoch in range(epochs):
+
+        for i, data in enumerate(dataloader, 0):
+            D.zero_grad()
+            real_img = data['image'].to(device)
+            input = torch.randn(real_img.shape[0], 100, 1, 1).to(device)
+            
